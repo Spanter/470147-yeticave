3ecabd34c67b61228344ed31e0ceb1b7762f7bd7
Maksim Ivanchenko <35325543+Spanter@users.noreply.github.com>
2018-05-19 02:59:32 +1000

    Исправил ошибки

--- queries.sql
+++ queries.sql
@@ -19,12 +19,12 @@ VALUES
 INSERT INTO lot
 (name, description, img, create_date, end_date, start_price, finish_price, category_id, win_id, author_id)
 VALUES
-('2015 Rossignol District Snowboard', 'Описание товара', 'img/lot-1.jpg', '2018-05-3 00:00:00', '2018-05-4 00:00:00', 1000, 10999, 1, 3, 1),
-('DC Ply Mens 2016/2017 Snowboard', 'Описание товара', 'img/lot-2.jpg', '2018-05-4 00:00:00', '2018-05-5 00:00:00', 2800, 15999, 1, 2, 3),
-('Крепления Union Contact Pro 2015 года размер L/XL', 'Описание товара', 'img/lot-3.jpg', '2018-05-15 00:00:00', '2018-06-3 00:00:00', 750, 8000, 2, 2, 4),
-('Ботинки для сноуборда DC Mutiny Charoca', 'Описание товара', 'img/lot-4.jpg', '2018-05-6 00:00:00', '2018-05-7 00:00:00', 990, 10999, 3, 1, 2),
-('Куртка для сноуборда DC Mutiny Charocal', 'Описание товара', 'img/lot-5.jpg', '2018-05-7 00:00:00', '2018-05-8 00:00:00', 700, 7500, 4, 1, 3),
-('Маска Oakley Canopy', 'Описание товара', 'img/lot-5.jpg', '2018-05-8 00:00:00', '2018-05-9 00:00:00', 500, 5400, 6, 4, 2);
+('2015 Rossignol District Snowboard', 'Описание товара', 'img/lot-1.jpg', '2018-05-3 00:00:00', '2018-06-4 00:00:00', 1000, 10999, 1, 3, 1),
+('DC Ply Mens 2016/2017 Snowboard', 'Описание товара', 'img/lot-2.jpg', '2018-05-4 00:00:00', '2018-06-5 00:00:00', 2800, 15999, 1, 2, 3),
+('Крепления Union Contact Pro 2015 года размер L/XL', 'Описание товара', 'img/lot-3.jpg', '2018-05-7 00:00:00', '2018-06-9 00:00:00', 750, 8000, 2, 2, 4),
+('Ботинки для сноуборда DC Mutiny Charoca', 'Описание товара', 'img/lot-4.jpg', '2018-05-9 00:00:00', '2018-06-7 00:00:00', 990, 10999, 3, 1, 2),
+('Куртка для сноуборда DC Mutiny Charocal', 'Описание товара', 'img/lot-5.jpg', '2018-05-7 00:00:00', '2018-06-8 00:00:00', 700, 7500, 4, 1, 3),
+('Маска Oakley Canopy', 'Описание товара', 'img/lot-5.jpg', '2018-05-8 00:00:00', '2018-06-10 00:00:00', 500, 5400, 6, 4, 2);
 
 /*** Добавление данных ставок ***/
 
@@ -53,12 +53,12 @@ UPDATE lot SET name = '2014 Rossignol District Snowboard' WHERE id = 1;
 
 /**** Показ лота  по id. Получение нназвание категорий по лоту***/
 
-SELECT name, create_date, img, start_price, end_date, finish_price, author_id, cat_name
-FROM lot JOIN categories using(id) WHERE id=1;
+SELECT name, lot.create_date, img, start_price, end_date, finish_price, author_id, cat_name
+FROM lot JOIN categories ON lot.category_id = categories.id WHERE lot.id=1;
 
 /*** получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, количество ставок, название категории ***/
 
-SELECT name, start_price, img, finish_price, COUNT(bet.id), cat_name
-FROM lot JOIN  bet using(id) JOIN categories using(id)
-WHERE NOW() < end_date
-GROUP BY id ORDER BY create_date ASC;
+SELECT name, start_price, img, finish_price,COUNT(bet.lot_id) AS bets, cat_name
+FROM lot JOIN  bet ON bet.lot_id = lot.id JOIN categories ON lot.category_id = categories.id
+WHERE NOW() < lot.end_date
+GROUP BY lot.id ORDER BY lot.create_date DESC;
